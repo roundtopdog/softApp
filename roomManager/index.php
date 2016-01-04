621b15f7bab51e4a743d03cd428d2d3d4219e938
will <wilfredrthibodeau@gmail.com>
2016-01-04 15:21:04 -0500

    added room_list

--- roomManager/index.php
+++ roomManager/index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>RoomManager</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
+<?php 
+include '../view/header.php';
+
+?>
+
 	<p>room some text</p>
 </body>
 </html>
